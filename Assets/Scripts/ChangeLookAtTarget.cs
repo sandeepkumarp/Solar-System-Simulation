99a51ff65a21a0a49af0c200832e0fa2650d8075
Sandeep Kumar Pilakhnawal <sandeep.pilakhnawal@gmail.com>
2016-03-24 12:59:04 +0530

    Added more planets, Textures, Sounds, UI, Code
    Project finalized for submission with a Web Player build.
    Also, Unity Version used is now 4.3.x

--- Assets/Scripts/ChangeLookAtTarget.cs
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -1,22 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class ChangeLookAtTarget : MonoBehaviour {
+public class ChangeLookAtTarget : MonoBehaviour
+{
 
-	public GameObject target; // the target that the camera should look at
+    public GameObject target; // the target that the camera should look at
+    public Text text;
 
-	void Start() {
-		if (target == null) 
-		{
-			target = this.gameObject;
-			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
-		}
-	}
+    void Start()
+    {
+        if (target == null)
+        {
+            target = this.gameObject;
+            Debug.Log("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+        }
+    }
 
-	// Called when MouseDown on this gameObject
-	void OnMouseDown () {
-		// change the target of the LookAtTarget script to be this gameobject.
-		LookAtTarget.target = target;
-		Camera.main.fieldOfView = 60*target.transform.localScale.x;
-	}
+    // Called when MouseDown on this gameObject
+    void OnMouseDown()
+    {
+        // change the target of the LookAtTarget script to be this gameobject.
+        LookAtTarget.target = target;
+        text.text = "Solar System\nFocusing on " + target.name;
+        Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+    }
 }
